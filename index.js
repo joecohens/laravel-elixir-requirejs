e02a89113f1a91e43bfd04abcad813f866738930
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-11-17 17:56:05 -0600

    Remove gulp ignore

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp'),
     rjs = require('gulp-requirejs'),
-    gulpIgnore = require('gulp-ignore'),
     _ = require('underscore'),
     Elixir = require('laravel-elixir');
 
